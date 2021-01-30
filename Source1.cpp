408de7770d2647789f081dc754a0a47e0a37ba74
UstinovIT <77569668+UstinovIT@users.noreply.github.com>
2021-01-30 18:09:45 +0300

    Add files via upload

---
+++ Source1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i;
+	int h;
+	for (i = 0; i < 5; i++)
+	{
+		cout << "#" << endl;
+	}
+	for (h = 0; h < 5; h++)
+	{
+		cout <<"#";
+	}
+	return 0;
+}
